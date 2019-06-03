ba3e728f715f9439e58aa96eaa413368a4a0bdd3
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-03 18:19:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 var recipes= {};
 
-/*function updateObjectWithKeyAndValue(object,key,value){
-return Object.assign({}, object,{key:[value]})
-  
-}*/
+<<<<<<< HEAD
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value })
 }
+=======
+function updateObjectWithKeyAndValue(object,key,value){
+ Object.assign({}, object,{key:[value]});
+   return object;
+}
+
+>>>>>>> 6da29f8bbc5833b8957584bcd5110044caba143d
 function destructivelyUpdateObjectWithKeyAndValue(object, key,value){
   object[key]=value;
   return object;
 }
 
 function deleteFromObjectByKey(object,key){
+<<<<<<< HEAD
 var obj={object: key};
 var newObj=Object.assign({}, obj);
 newObj;
 delete newObj.key;
 newObj;
 return obj;
+=======
+>>>>>>> 6da29f8bbc5833b8957584bcd5110044caba143d
 
 }
 
